b1b8ce64a528c613c7154d5ae96159ee8af299d0
Andre <andreferreira@live.co.uk>
2024-04-03 21:56:00 +0100

    first commit

---
+++ CoreComponents/App.js
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#1c1c1c" }}>
+      <View style={{ width: 200, height: 200, backgroundColor: "plum" }}></View>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+      ></View>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
+      ></View>
+    </View>
+  );
+}
